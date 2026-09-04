9b2b2ef46850a151f298a75f54fd92fb13db7309
Eliana Rodriguez <rodriguezabigail633@gmail.com>
2026-09-04 14:48:15 -0300

    intento 1 de arreglar urls.py

--- hamburgueseria_project/gestion/urls.py
+++ hamburgueseria_project/gestion/urls.py
@@ -2,23 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
-=======
     # --- RUTAS DE INSUMOS ---
->>>>>>> 561a280 (fijate ahora)
     path('insumos/', views.listar_insumos, name='listar_insumos'),
-    path('insumos/inactivos/', views.listar_insumos_inactivos, name='listar_insumos_inactivos'), # <-- RUTA QUE FALTABA
+    path('insumos/inactivos/', views.listar_insumos_inactivos, name='listar_insumos_inactivos'), 
     path('insumos/nuevo/', views.crear_insumo, name='crear_insumo'),
     path('insumos/detalle/<int:id>/', views.detalle_insumo, name='detalle_insumo'),
     path('insumos/editar/<int:id>/', views.editar_insumo, name='editar_insumo'),
     path('insumos/desactivar/<int:id>/', views.desactivar_insumo, name='desactivar_insumo'),
     path('insumos/activar/<int:id>/', views.activar_insumo, name='activar_insumo'),
-<<<<<<< HEAD
-=======
 
     # --- RUTAS DE PRODUCTOS ---
     path('productos/', views.listar_productos, name='listar_productos'),
     path('productos/nuevo/', views.crear_producto, name='crear_producto'),
     path('productos/<int:producto_id>/ingredientes/', views.gestionar_ingredientes, name='gestionar_ingredientes'),
->>>>>>> 561a280 (fijate ahora)
 ]
